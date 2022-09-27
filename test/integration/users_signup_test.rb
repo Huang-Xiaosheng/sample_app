48015debd156c764dc08bdb81041599da31cba9e
Huang Xiaosheng <Huang-Xiaosheng@outlook.com>
2022-09-27 23:05:13 +0800

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,6 +24,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
+    assert_template 'users/show'
     refute_empty flash
+    assert is_logged_in?
   end
 end
