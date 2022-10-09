97b8614245688d7e6df2a6b079dd673bfdec3648
Huang Xiaosheng <Huang-Xiaosheng@outlook.com>
2022-10-09 22:35:03 +0800

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -83,4 +83,36 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    refute michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    refute michael.following?(archer)
+    # Users can't follow themselves.
+    michael.follow(michael)
+    refute michael.following?(michael)
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Self-posts for user with followers
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from non-followed user
+    archer.microposts.each do |post_unfollowed|
+      refute michael.feed.include?(post_unfollowed)
+    end
+  end
 end
